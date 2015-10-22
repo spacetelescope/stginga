439c99a058c51787ca755fd84430594f4cfbcac3
J.D.Eisenhamer <eisenhamer@stsci.edu>
2015-10-22 15:54:39 -0400

    Initial framework for a multi-image viewer

---
+++ ginga/qtw/plugins/MultiImage.py
@@ -0,0 +1,360 @@
+from ginga import GingaPlugin
+from ginga.gw import Widgets, Viewers
+
+
+class MultiImage(GingaPlugin.LocalPlugin):
+    """Coordinate display between multiple images"""
+
+    def __init__(self, fv, fitsimage):
+        super(MultiImage, self).__init__(fv, fitsimage)
+
+        self.dc = self.fv.getDrawClasses()
+
+        canvas = self.dc.DrawingCanvas()
+        canvas.enable_draw(True)
+        canvas.enable_edit(True)
+        canvas.set_drawtype('rectangle', color='cyan', linestyle='dash',
+                            drawdims=True)
+        canvas.set_callback('draw-event', self.draw_cb)
+        canvas.set_callback('edit-event', self.edit_cb)
+        canvas.add_draw_mode('move', down=self.btndown,
+                             move=self.drag, up=self.update)
+        canvas.register_for_cursor_drawing(self.fitsimage)
+        canvas.setSurface(self.fitsimage)
+        canvas.set_draw_mode('move')
+        self.canvas = canvas
+
+        self.layertag = 'muimg-canvas'
+        self.dx = 30
+        self.dy = 30
+        self.pickcolor = 'green'
+        self.max_side = 1024
+
+    def build_gui(self, container):
+        """Build the Dialog"""
+
+        # Get container specs.
+        vbox, sw, orientation = Widgets.get_oriented_box(container)
+        vbox.set_border_width(4)
+        vbox.set_spacing(2)
+
+        # Overall container
+        vtop = Widgets.VBox()
+        vtop.set_border_width(4)
+        vtop.add_widget(sw, stretch=1)
+
+        # Instructiopns
+        self.msgFont = self.fv.getFont("sansFont", 12)
+        tw = Widgets.TextArea(wrap=True, editable=False)
+        tw.set_font(self.msgFont)
+        self.tw = tw
+
+        fr = Widgets.Expander("Instructions")
+        fr.set_widget(tw)
+        vbox.add_widget(fr, stretch=0)
+
+        # Setup for thumbnail display
+        cm, im = self.fv.cm, self.fv.im
+        di = Viewers.ImageViewCanvas(logger=self.logger)
+        width, height = 200, 200
+        di.configure_window(width, height)
+        di.enable_autozoom('off')
+        di.enable_autocuts('off')
+        di.zoom_to(3)
+        settings = di.get_settings()
+        settings.getSetting('zoomlevel').add_callback('set',
+                                                      self.zoomset, di)
+        di.set_cmap(cm)
+        di.set_imap(im)
+        di.set_callback('none-move', self.detailxy)
+        di.set_bg(0.4, 0.4, 0.4)
+        # for debugging
+        di.set_name('pickimage')
+        self.pickimage = di
+
+        bd = di.get_bindings()
+        bd.enable_pan(True)
+        bd.enable_zoom(True)
+        bd.enable_cuts(True)
+
+        iw = Widgets.wrap(di.get_widget())
+        vtop.add_widget(iw)
+
+        container.add_widget(vtop, stretch=1)
+
+    def instructions(self):
+        self.tw.set_text('These would be fun instructions.')
+        self.tw.set_font(self.msgFont)
+
+    def start(self):
+        self.instructions()
+
+        # insert layer if it is not already
+        p_canvas = self.fitsimage.get_canvas()
+        try:
+            p_canvas.getObjectByTag(self.layertag)
+
+        except KeyError:
+            # Add canvas layer
+            p_canvas.add(self.canvas, tag=self.layertag)
+
+        self.resume()
+
+    def resume(self):
+        # turn off any mode user may be in
+        #self.modes_off()
+
+        self.canvas.ui_setActive(True)
+        self.fv.showStatus("Do something")
+
+    def redo(self):
+        fig = self.canvas.getObjectByTag(self.picktag)
+        if fig.kind != 'compound':
+            return True
+        bbox  = fig.objects[0]
+        point = fig.objects[1]
+        text = fig.objects[2]
+        data_x, data_y = point.x, point.y
+
+        # set the pick image to have the same cut levels and transforms
+        self.fitsimage.copy_attributes(self.pickimage,
+                                       ['transforms', 'cutlevels',
+                                        'rgbmap'])
+
+        try:
+            image = self.fitsimage.get_image()
+
+            # sanity check on region
+            width = bbox.x2 - bbox.x1
+            height = bbox.y2 - bbox.y1
+            if (width > self.max_side) or (height > self.max_side):
+                errmsg = "Image area (%dx%d) too large!" % (
+                    width, height)
+                self.fv.show_error(errmsg)
+                raise Exception(errmsg)
+
+            # Cut and show pick image in pick window
+            self.logger.debug("bbox %f,%f %f,%f" % (bbox.x1, bbox.y1,
+                                                    bbox.x2, bbox.y2))
+            x1, y1, x2, y2, data = self.cutdetail(self.fitsimage,
+                                                  self.pickimage,
+                                                  int(bbox.x1), int(bbox.y1),
+                                                  int(bbox.x2), int(bbox.y2))
+            self.logger.debug("cut box %f,%f %f,%f" % (x1, y1, x2, y2))
+
+        except Exception as e:
+            self.logger.error("Error calculating quality metrics: %s" % (
+                str(e)))
+            return True
+
+    def stop(self):
+        # deactivate the canvas
+        self.canvas.ui_setActive(False)
+        self.fv.showStatus("")
+
+    def close(self):
+        chname = self.fv.get_channelName(self.fitsimage)
+        self.fv.stop_local_plugin(chname, str(self))
+        return True
+
+    def pause(self):
+        self.canvas.ui_setActive(False)
+
+    def __str__(self):
+        return 'multi image'
+
+    def zoomset(self, setting, zoomlevel, fitsimage):
+        scalefactor = fitsimage.get_scale()
+        self.logger.debug("scalefactor = %.2f" % (scalefactor))
+        text = self.fv.scale2text(scalefactor)
+        self.wdetail.zoom.set_text(text)
+
+    def btndown(self, canvas, event, data_x, data_y, viewer):
+        try:
+            obj = self.canvas.getObjectByTag(self.picktag)
+            if obj.kind == 'rectangle':
+                bbox = obj
+            else:
+                bbox = obj.objects[0]
+                point = obj.objects[1]
+            self.dx = (bbox.x2 - bbox.x1) // 2
+            self.dy = (bbox.y2 - bbox.y1) // 2
+        except Exception as e:
+            pass
+
+        dx = self.dx
+        dy = self.dy
+
+        # Mark center of object and region on main image
+        try:
+            self.canvas.deleteObjectByTag(self.picktag)
+        except:
+            pass
+
+        x1, y1 = data_x - dx, data_y - dy
+        x2, y2 = data_x + dx, data_y + dy
+
+        tag = self.canvas.add(self.dc.Rectangle(x1, y1, x2, y2,
+                                                color='cyan',
+                                                linestyle='dash'))
+        self.picktag = tag
+
+        #self.draw_cb(self.canvas, tag)
+        return True
+
+    def update(self, canvas, event, data_x, data_y, viewer):
+        try:
+            obj = self.canvas.getObjectByTag(self.picktag)
+            if obj.kind == 'rectangle':
+                bbox = obj
+            else:
+                bbox = obj.objects[0]
+                point = obj.objects[1]
+            self.dx = (bbox.x2 - bbox.x1) // 2
+            self.dy = (bbox.y2 - bbox.y1) // 2
+        except Exception as e:
+            obj = None
+            pass
+
+        dx = self.dx
+        dy = self.dy
+
+        x1, y1 = data_x - dx, data_y - dy
+        x2, y2 = data_x + dx, data_y + dy
+
+        if (not obj) or (obj.kind == 'compound'):
+            # Replace compound image with rectangle
+            try:
+                self.canvas.deleteObjectByTag(self.picktag)
+            except:
+                pass
+
+            tag = self.canvas.add(self.dc.Rectangle(x1, y1, x2, y2,
+                                                    color='cyan',
+                                                    linestyle='dash'))
+        else:
+            # Update current rectangle with new coords
+            bbox.x1, bbox.y1, bbox.x2, bbox.y2 = x1, y1, x2, y2
+            tag = self.picktag
+
+        self.draw_cb(self.canvas, tag)
+        return True
+
+    def drag(self, canvas, event, data_x, data_y, viewer):
+
+        obj = self.canvas.getObjectByTag(self.picktag)
+        if obj.kind == 'compound':
+            bbox = obj.objects[0]
+        elif obj.kind == 'rectangle':
+            bbox = obj
+        else:
+            return True
+
+        # calculate center of bbox
+        wd = bbox.x2 - bbox.x1
+        dw = wd // 2
+        ht = bbox.y2 - bbox.y1
+        dh = ht // 2
+        x, y = bbox.x1 + dw, bbox.y1 + dh
+
+        # calculate offsets of move
+        dx = (data_x - x)
+        dy = (data_y - y)
+
+        # calculate new coords
+        x1, y1, x2, y2 = bbox.x1+dx, bbox.y1+dy, bbox.x2+dx, bbox.y2+dy
+
+        if (not obj) or (obj.kind == 'compound'):
+            # Replace compound image with rectangle
+            try:
+                self.canvas.deleteObjectByTag(self.picktag)
+            except:
+                pass
+
+            self.picktag = self.canvas.add(self.dc.Rectangle(x1, y1, x2, y2,
+                                                             color='cyan',
+                                                             linestyle='dash'))
+        else:
+            # Update current rectangle with new coords and redraw
+            bbox.x1, bbox.y1, bbox.x2, bbox.y2 = x1, y1, x2, y2
+            self.canvas.redraw(whence=3)
+
+        return True
+
+    def draw_cb(self, canvas, tag):
+        obj = canvas.getObjectByTag(tag)
+        if obj.kind != 'rectangle':
+            return True
+        canvas.deleteObjectByTag(tag)
+
+        if self.picktag:
+            try:
+                canvas.deleteObjectByTag(self.picktag)
+            except:
+                pass
+
+        # determine center of rectangle
+        x1, y1, x2, y2 = obj.get_llur()
+        x = x1 + (x2 - x1) // 2
+        y = y1 + (y2 - y1) // 2
+
+        tag = canvas.add(self.dc.CompoundObject(
+            self.dc.Rectangle(x1, y1, x2, y2,
+                              color=self.pickcolor),
+            self.dc.Point(x, y, 10, color='red'),
+            self.dc.Text(x1, y2+4, "Pick: calc",
+                         color=self.pickcolor)))
+        self.picktag = tag
+
+        #self.fv.raise_tab("detail")
+        return self.redo()
+
+    def edit_cb(self, canvas, obj):
+        if obj.kind != 'rectangle':
+            return True
+
+        # Get the compound object that sits on the canvas.
+        # Make sure edited rectangle was our pick rectangle.
+        c_obj = self.canvas.getObjectByTag(self.picktag)
+        if (c_obj.kind != 'compound') or (len(c_obj.objects) < 3) or \
+           (c_obj.objects[0] != obj):
+            return False
+
+        # determine center of rectangle
+        x1, y1, x2, y2 = obj.get_llur()
+        x = x1 + (x2 - x1) // 2
+        y = y1 + (y2 - y1) // 2
+
+        # reposition other elements to match
+        point = c_obj.objects[1]
+        point.x, point.y = x, y
+        text = c_obj.objects[2]
+        text.x, text.y = x1, y2 + 4
+
+        return self.redo()
+
+    def detailxy(self, canvas, button, data_x, data_y):
+        """Motion event in the pick fits window.  Show the pointing
+        information under the cursor.
+        """
+        if button == 0:
+            # TODO: we could track the focus changes to make this check
+            # more efficient
+            fitsimage = self.fv.getfocus_fitsimage()
+            # Don't update global information if our fitsimage isn't focused
+            if fitsimage != self.fitsimage:
+                return True
+
+            # Add offsets from cutout
+            data_x = data_x + self.pick_x1
+            data_y = data_y + self.pick_y1
+
+            return self.fv.showxy(self.fitsimage, data_x, data_y)
+
+    def cutdetail(self, srcimage, dstimage, x1, y1, x2, y2):
+        image = srcimage.get_image()
+        data, x1, y1, x2, y2 = image.cutout_adjust(x1, y1, x2, y2)
+
+        dstimage.set_data(data)
+
+        return (x1, y1, x2, y2, data)
