5615985eb0e420ab6dfa4e7a27d47ec80964fd6d
Pey Lian Lim <lim@stsci.edu>
2015-10-30 13:41:59 -0400

    Fixed setup version warning. Removed unused misc directory. Fixed DQInspect plugin.

--- stginga/__init__.py
+++ stginga/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-__version__ = '0.1.0'
+__version__ = '0.1.0.dev0'
 
 from .plugin_info import *
