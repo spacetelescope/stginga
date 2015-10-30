01a783b7f09e527f80e250a53373486e9b77fbe9
Erik Tollerud <erik.tollerud@gmail.com>
2015-10-30 16:16:58 -0400

    working nbinteract prototype

--- stginga/nbinteract.py
+++ stginga/nbinteract.py
@@ -2,19 +2,39 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
+import os
+
 from astropy.io import fits
 from astropy.utils import isiterable
+
+import tornado.httpserver
+import tornado.web
+import tornado.ioloop
+
+from ginga.misc import log, Task
 from ginga.AstroImage import AstroImage
+from ginga.web.pgw import Widgets, js, PgHelp, ipg
 
 __all__ = ['start_server', 'GingaServer', '']
 
 
-# used for some of the stateful functions below
-_server_singleton = None
-
 class GingaServer(object):
-    def __init__(self):
-        self.url = None
+    def __init__(self, host='localhost', port=9909, logger=None, numthreads=5):
+        self.tornado_app = None
+        self.viewers = {}
+        self.host = host
+        self.port = port
+
+        if logger is None:
+            logger = log.get_logger("nbinteract_server", null=True)
+        self.logger = logger
+
+        self.thread_pool = Task.ThreadPool(numthreads, logger)
+        self.app = Widgets.Application(logger=self.logger, base_url=self.base_url)
+
+    @property
+    def base_url(self):
+        return "http://{0}:{1}/app".format(self.host, self.port)
 
     def __repr__(self):
         repr_str = object.__repr__(self)
@@ -25,58 +45,63 @@ class GingaServer(object):
         return repr_str.replace('object at', urlstr + ' at')
 
 
-    def start(self):
-    self.url = 'No URL'
+    def start(self, create_main_window=True):
+        self.thread_pool.startall()
+
+        #TODO: DONT DO THIS.  Use package data instead
+        js_path = os.path.dirname(js.__file__)
+
+        self.tornado_app = tornado.web.Application([
+            (r"/js/(.*\.js)", tornado.web.StaticFileHandler,
+             {"path":  js_path}),
+            (r"/app", PgHelp.WindowHandler,
+              dict(name='Application', url='/app', app=self.app)),
+            (r"/app/socket", PgHelp.ApplicationHandler,
+              dict(name='Ginga', app=self.app)),
+            ], logger=self.logger)
+
+
+        self.tornado_server = tornado.httpserver.HTTPServer(self.tornado_app)
+        self.tornado_server.listen(self.port, self.host)
+
+        if create_main_window:
+            self.new_viewer('Main Viewer')
 
     def stop(self):
-    global _server_singleton
+        raise NotImplementedError
 
-    self.url = None
-    if _server_singleton is self:
-        _server_singleton = None
+    def new_viewer(self, viewer_name,):
+        """
+        Create a new viewer with the given name
+        """
+        if viewer_name in self.viewers:
+            raise ValueError('Viewer {} already exists'.format(viewer_name))
 
+        # our own viewer object, customized with methods (see above)
+        self.viewers[viewer_name] = ipg.ImageViewer(self.logger, self.app.make_window(viewer_name))
+        return self.viewers[viewer_name]
 
-def start_server(server=None):
-    global _server_singleton
-
-    if server is None:
-        server = GingaServer()
-
-    server.start()
-    if _server_singleton is None:
-        _server_singleton = server
-    return server
+    def get_viewer_urls(self):
+        return {name: viewer.top.url  for name, viewer in self.viewers.items()}
 
 
-def load_fits_file(fileorhdu, server=None):
-    if isinstance(fileorhdu,f ile):
-        fileorhdu = fits.HDUList.fromfile(fileorhdu)
+    def load_fits_file(self, fileorhdu, viewer_name='Main Viewer'):
+        if isinstance(fileorhdu, file):
+            fileorhdu = fits.HDUList.fromfile(fileorhdu)
 
-    if isiterable(fileordu):
-        for hdui in fileorhdu:
-            if hasattr(hdu, 'is_image') and hdu.is_image:
-                hdu = hdui
-                break
+        if isiterable(fileorhdu):
+            for hdui in fileorhdu:
+                if hasattr(hdu, 'is_image') and hdu.is_image:
+                    hdu = hdui
+                    break
+            else:
+                raise ValueError('fileorhdu was iterable but did not contain any image HDUs')
+        elif hasattr(fileorhdu, 'data') and hasattr(fileorhdu,'header'):
+            #quacks like an HDU - give it a shot
+            hdu = fileorhdu
         else:
-            raise ValueError('fileorhdu was iterable but did not contain any image HDUs')
-    elif hasattr(fileorhdu, 'data') and hasattr(fileorhdu,'header'):
-        #quacks like an HDU - give it a shot
-        hdu = fileorhdu
-    else:
-        raise ValueError('fileorhdu was not a fits file or HDU-ish thing')
+            raise ValueError('fileorhdu was not a fits file or HDU-ish thing')
 
-    server = _get_default_server(server)
-
-    aim = AstroImage(logger=image.logger)
-    aim.load_hdu(hdu)
-    server.set_image(aim)
-
-
-def _get_default_server(server):
-    if server is None:
-        if _server_singleton is None:
-            raise ValueError('No server has been started, cannot identify the default.')
-        else:
-            return _server_singleton
-    else:
-        return server
+        aim = AstroImage(logger=self.logger)
+        aim.load_hdu(hdu)
+        self.viewers[viewer_name].fitsimage.set_image(aim)
