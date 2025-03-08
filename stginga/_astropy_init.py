8ac37833e9e0785aa11315f41bcca4f44e21c773
P. L. Lim <2090236+pllim@users.noreply.github.com>
2025-03-07 21:12:00 -0500

    Remove astropy test runner

--- stginga/_astropy_init.py
+++ stginga/_astropy_init.py
@@ -1,16 +1,8 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-import os
-
-from astropy.tests.runner import TestRunner
-
 try:
     from .version import version as __version__
 except ImportError:
     __version__ = 'unknown'
 __vdate__ = '2019-12-29'
 
-# Create the test function for self test
-test = TestRunner.make_test_runner_in(os.path.dirname(__file__))
-test.__test__ = False
-
-__all__ = ['__version__', 'test']
+__all__ = ['__version__']
